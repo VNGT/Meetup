694b92133f6ec210a46a07bbf06de1b65b69f04e
Thinh Nguyen <tnntech@gatech.edu>
2020-01-27 09:38:01 -0500

    update eslint strict type. Add pre-push command

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,31 @@
 module.exports = {
-    extends: ['airbnb'],
-    rules: {
-        'react/jsx-filename-extension': 'off',
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
     },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
 };
