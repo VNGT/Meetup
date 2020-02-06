b007b9c255446ba2ade1d677839c085daea9d2fc
Thinh Nguyen <tnntech@gatech.edu>
2020-02-05 23:09:22 -0500

    add account api endpoint

--- server-side/src/controllers/account.controller.js
+++ server-side/src/controllers/account.controller.js
@@ -1,29 +1,51 @@
+const path = require('path');
+const uuid = require('uuid');
+const shortid = require('shortid');
 const Account = require('../models/account.model.js');
+const { doclient } = require('../config/database');
+const RequestCall = require('../utils/request.util');
+const statusCode = require('../utils/response.util');
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
-exports.getAccountById = (async () => {
-	return {
-		statusCode: 200,
-		body: JSON.stringify({message: 'Get Account of Client by ID'}),
-	};
+// Intilize account class request
+const accountReq = new RequestCall(doclient, process.env.ACCOUNTS_TABLE);
+
+// Status Code
+const ok = statusCode.withStatusCode(200, JSON.stringify);
+const error = statusCode.withStatusCode(500, JSON.stringify);
+
+exports.getAccountById = (async (event) => {
+	const { acctId } = event.pathParameters;
+	const data = await accountReq.get(acctId);
+	if (data === undefined || !data) {
+		return error({message: 'Can not find ID'});
+	}
+	return ok(data);
 });
 
-exports.deleteAccountById = (async () => {
-	return {
-		statusCode: 200,
-		body: JSON.stringify({message: 'Delete Account by ID'}),
-	};
+exports.deleteAccountById = (async (event) => {
+	const { acctId } = event.pathParameters;
+	const idDelete = await accountReq.delete(acctId);
+	if (!idDelete) { return ok(idDelete); }
+	return error({message: 'Can not find ID'});
 });
 
-exports.updateAccountById = (async () => {
-	return {
-		statusCode: 200,
-		body: JSON.stringify({message: 'Update Account by ID'}),
-	};
+exports.updateAccountById = (async (event) => {
+	const { pathParameters, body } = event;
+	const { acctId } = pathParameters;
+	const updatedObject = await accountReq.put(body, acctId);
+	if (!updatedObject) { return ok({message: `${acctId} updated`}); }
+	return error({message: 'Can not find ID'});
 });
 
-exports.createAccount = (async () => {
-	return {
-		statusCode: 200,
-		body: JSON.stringify({message: 'Create an Account'}),
+exports.createAccount = (async (event) => {
+	const { body } = event;
+	const extraParams = {
+		hostId: shortid.generate(),
+		id: uuid()
 	};
+	const object = Object.assign({}, extraParams, JSON.parse(body));
+	const newAccount = new Account(object);
+	await accountReq.post(newAccount, uuid());
+	return ok({message: 'New Account created'});
 });
--- server-side/src/utils/request.util.js
+++ server-side/src/utils/request.util.js
@@ -1,64 +1,41 @@
 class RequestCall {
 
-	/**
-	 * Contructs a new contact repository
-	 * @param {AWS.DynamoDB.DocumentClient} documentClient The Document Client
-	 */
 	constructor(documentClient, tableName) {
 		this._documentClient = documentClient;
-		this._tableName = {TableName: tableName};
+		this._tableName = { TableName: tableName };
 	}
 
-	/**
-	 * Gets a list of contacts
-	 * @returns {Promise<Models.Contact[]>} A list of contacts
-	 */
 	async list() {
 		const params = this._createParamObject();
 		const response = await this._documentClient.scan(params).promise();
 		return response.Items || [];
 	}
 
-	/**
-	 * Gets a contact by id
-	 * @param {string} id The contact id
-	 * @returns {Promise<Models.Contact>} The contact
-	 */
 	async get(id) {
 		const params = this._createParamObject({ Key: { id } });
 		const response = await this._documentClient.get(params).promise();
 		return response.Item;
 	}
 
-	/**
-	 * Add or replace a object
-	 * @param {Models.object} object The object
-	 * @returns {Promise<Models.object>} The object
-	 */
-	async put(object) {
+	async put(object, id) {
 		const params = this._createParamObject({ Item: object });
+		const foundID = await this.get(id);
+		if (!foundID || foundID === undefined) { return null; }
 		await this._documentClient.put(params).promise();
 		return object;
 	}
 
-	/**
-	 * Add a object
-	 * @param {Models.object} object The object
-	 * @returns {Promise<Models.object>} The object
-	 */
-	async post(object) {
-		const params = this._createParamObject({ Item: object });
-		await this._documentClient.post(params).promise();
+	async post(object, id) {
+		const params = this._createParamObject({ Key: id, Item: object, Expected: {	email: { Exists: false }} });
+		const response = await this._documentClient.put(params).promise();
+		console.log(response);
 		return object;
 	}
 
-	/**
-	 * Deletes a contact by id
-	 * @param {string} id The contact id
-	 * @return {Promise<string>} The id of the deleted contact
-	 */
 	async delete(id) {
 		const params = this._createParamObject({ Key: { id } });
+		const foundId = await this.get(id);
+		if (!foundId || foundId === undefined) { return null; }
 		await this._documentClient.delete(params).promise();
 		return id;
 	}
