5f4fde62341c546ac243f6c6b6e911180409adb1
Tuan Nguyen <atuannguyen1101@gmail.com>
2020-02-23 16:09:47 -0500

    Remove lint temporately

--- server-side/src/controllers/event.controller.js
+++ server-side/src/controllers/event.controller.js
@@ -1,10 +1,20 @@
 const Event = require('../models/event.model.js');
+const { doclient } = require('../config/database');
+const RequestCall = require('../utils/request.util');
+const statusCode = require('../utils/response.util');
+const shortid = require('shortid');
 
-exports.createEvent = (async() => {
-	return {
-		statusCode: 200,
-		body: JSON.stringify({message: 'Create an Event'}),
-	};
+const eventReq = new RequestCall(doclient, process.env.EVENTS_TABLE);
+const ok = statusCode.withStatusCode(200, JSON.stringify);
+const error = statusCode.withStatusCode(500, JSON.stringify);
+
+exports.createEvent = (async(event) => {
+	const { body } = event;
+	const id = shortid.generate()
+	const object = Object.assign({}, {id: id}, JSON.parse(body))
+	const addNewStatus = await eventReq.post(object, id)
+	if (addNewStatus) { return ok({message: 'New Event created'}); }
+	return error({ message: 'Event already exist' });
 });
 
 exports.getEventById = (async() => {
