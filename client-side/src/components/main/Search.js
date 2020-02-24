28acf7038accb9904cb056485a5007d2e155aefe
Lan Le Tu <lan.letu96@gmail.com>
2020-02-23 19:49:34 -0500

    Search page

--- client-side/src/components/main/Search.js
+++ client-side/src/components/main/Search.js
@@ -1,30 +1,68 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import styles from '../../styles/search.style';
-import CardWithField from '../../directives/CardWithField';
 import NavigationFooter from '../../directives/NavigationFooter';
+import { TextInput, Text, Searchbar } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+Icon.loadFont(); 
 
 class Search extends Component {
+	state = { 
+		searchValue: '',
+	};
 
-	// static navigationOptions = {
-	// 	header: null
-	// };
+	static navigationOptions = {
+		header: null
+	};
 
 	constructor(props) {
 		super(props);
-		this.state = {  };
+		this.state = { };
 	}
+
+	Title = () => <Text style={styles.title}>Search</Text>;
+
+	RecentSearch = () => <Text style={styles.recentSearch}>Recent Searches</Text>;
+
+	SearchField = () => (
+		<View>
+			{/* <Searchbar
+				placeholder="Search"
+				onChangeText={query => { this.setState({ searchValue: query }); }}
+				value={this.state.searchValue}
+			/> */}
+		</View>
+	);
+
+	getRecentSearches() {
+
+	}
+
+	ResentSearchList = () => (
+		<View style={styles.recentList}>
+			<FlatList
+				data={[
+					{key: 'CS 1331 Final'},
+					{key: 'CS 1332 Midterm'},
+					{key: 'Mock Interview'},
+				]}
+				renderItem={({item}) => <Text style={styles.recentItem}>{item.key}</Text>}
+			/>
+		</View>
+	);
 
 	render() {
 		return (
 			<View style={styles.view}>
-				
-				{/* <CardWithField setCard={1}/> */}
+				<this.Title/>
+				<this.SearchField/>
+				<this.RecentSearch/>
+				<this.ResentSearchList/>
 				<NavigationFooter/>
 			</View>
 		);
 	}
 }
 
-export default Search;
+export default withNavigation(Search);
--- client-side/src/styles/search.style.js
+++ client-side/src/styles/search.style.js
@@ -3,8 +3,41 @@ import Dimensions from '../services/Dimensions';
 
 export default StyleSheet.create({
 	view: {
-		backgroundColor: '#C3C3C3',
-		height: '100%'
+		flex: 1,
+		backgroundColor: 'white'
 	},
+	searchField: {
+		marginTop: 20,
+		flexDirection: 'row',
+	},
+	textInput: {
+		height: Dimensions.height / 20,
+		width: Dimensions.width / 6 * 5,
+		marginLeft: '5%'
+	},
+	title: {
+		marginTop: '10%',
+		fontWeight: 'bold',
+		fontSize: 50,
+		marginLeft: '7%'
+	},
+	recentSearch: {
+		marginTop: '10%',
+		fontWeight: 'bold',
+		fontSize: 20,
+		marginLeft: '7%'
+	},
+	recentList: {
+		marginTop: '5%',
+		fontSize: 15,
+		marginLeft: '7%',
+		borderTopColor: 'black',
+		marginVertical: 8,
+		marginHorizontal: 16,
+	},
+	recentItem: {
+		fontSize: 15,
+		padding: 10
+	}
 
 });
